aaf02da6a8e27c1849dbb945209c3fb0c6b18c02
manesakae <manesakae@gmail.com>
2023-07-27 14:59:36 -0500

    added react and graphql with apollo/client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -87,6 +87,11 @@ const mutation = new GraphQLObjectType({
             type: ClientType,
             args: { id: { type: GraphQLNonNull(GraphQLID) } },
             resolve(parent, args) {
+                Project.find({ clientId: args.id }).then((projects) => {
+                    projects.forEach(project => {
+                        project.deleteOne();
+                    })
+                })
                 return Client.findByIdAndRemove(args.id)
             }
         },
